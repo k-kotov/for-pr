9d731098daf6f5359c23657c4ab225b3e71130b0
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-06-04 12:28:51 +0300

    Create 1.tf

---
+++ 1.tf
@@ -0,0 +1,21 @@
+variable "run_id" {
+  default = "6"
+  #type = list(string)
+  #nullable = false
+  
+}
+
+
+variable "sleep_time" {
+  default = 60
+}
+
+resource "null_resource" "resource2" {
+  count = 5
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello World!"
+    }
+ }
+}
