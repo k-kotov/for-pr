fe0c0a359abfcdfa23166293e941e6aa7b5646e0
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-10-07 17:36:22 +0300

    Update 1.tf

--- 1.tf
+++ 1.tf
@@ -1,20 +1,38 @@
-variable "run_id" {
-  default = "56"
-sensitive  = true
-  
+terraform {
+required_version = ">= 1.0"
 }
 
 
-variable "sleep_time" {
-  default = 60
+# This template intentionally creates a large number of items via range()
+# to reproduce the Terraform error:
+# "Call to function \"range\" failed: more than 1024 values were generated"
+
+
+locals {
+# Increase to any value > 1024 to reproduce the error (e.g. 1500)
+input_count = 1500
+
+
+inputs = [for i in range(local.input_count) : {
+idx = i
+name = "item-${i}"
+}]
 }
 
-resource "null_resource" "resource2" {
-  count = 5
-  provisioner "local-exec" {
-    command = "echo $ENV"
-    environment = {
-      ENV = "Hello World!"
-    }
- }
+
+resource "null_resource" "repro" {
+# use for_each so Terraform evaluates the locals/range expression
+for_each = { for item in local.inputs : item.idx => item }
+
+
+triggers = {
+idx = each.value.idx
+name = each.value.name
+}
+
+
+provisioner "local-exec" {
+# no-op: just demonstrate resource creation if it ever proceeds
+command = "echo creating ${each.value.name}"
+}
 }
